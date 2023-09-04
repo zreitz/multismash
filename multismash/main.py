4f213f306093953be36ba4948bf66d493df40d17
zreitz <zlreitz@gmail.com>
2023-09-04 18:30:12 +0200

    Many improvements
    TODO: readme, logging

--- multismash/main.py
+++ multismash/main.py
@@ -1,7 +1,10 @@
 import argparse
-import textwrap
 import subprocess
+import textwrap
+import yaml
+
 from pathlib import Path
+from snakemake.utils import validate
 from typing import List, Tuple
 
 
@@ -28,33 +31,81 @@ def parse_args() -> Tuple[argparse.Namespace, List[str]]:
     return parser.parse_known_args()
 
 
+def install_bigscape(configs):
+    # If a bigscape environment isn't defined then download the repository
+    conda_dir = Path(__file__).parents[1] / "conda"
+    bigscape_dir = conda_dir / "BiG-SCAPE-1.1.5"
+    if not configs["bigscape_conda_env_name"] and not bigscape_dir.exists():
+        conda_dir.mkdir(exist_ok=True)
+        tar = Path(str(bigscape_dir) + ".tar.gz")
+        if not tar.exists():
+            input(f"BiG-SCAPE will be downloaded to {bigscape_dir}/. Press Enter to continue.")
+            url = "https://github.com/medema-group/BiG-SCAPE/archive/refs/tags/v1.1.5.tar.gz"
+            subprocess.run(["curl", url, "-L", "-o", tar], check=True)
+        subprocess.run(["tar", "-xzf", tar, "-C", conda_dir], check=True)
+        assert(bigscape_dir.exists())
+        tar.unlink()    # Delete
+
+    # check if the Pfam files exist, and if not, download them
+    pfam_dir = configs["pfam_dir"]
+    if pfam_dir:
+        pfam = Path(args.configfile).parents[0] / Path(pfam_dir)
+        pfam = pfam.expanduser().resolve()
+    else:
+        pfam = Path(__file__).parents[1] / "pfam"
+    files = [pfam / ("Pfam-A.hmm" + ext) for ext in ["", ".gz", ".h3f", ".h3i", ".h3m", ".h3p"]]
+
+    # Only download if needed and after prompt
+    if not files[0].exists():  # Pfam-A.hmm
+        if not files[1].exists():  # Pfam-A.hmm.gz
+            Path.mkdir(pfam, exist_ok=True)
+            print("Downloading the Pfam database from EBI...")
+            url = "https://ftp.ebi.ac.uk/pub/databases/Pfam/current_release/Pfam-A.hmm.gz"
+            subprocess.run(["curl", "-o", files[1], url], check=True)
+        subprocess.run(["gunzip", files[1]], check=True)
+
+    if not all((file.exists() for file in files[2:])):
+        print("Pressing the Pfam database...")
+        subprocess.run(["hmmpress", files[0]], check=True)
+        print("Done")
+
+
 def main():
     args, snakemake_args = parse_args()
 
-    if "--configfile" in snakemake_args:
-        raise SystemExit("Error: do not use the --configfile flag with multiSMASH")
+    forbidden = {"--snakefile", "--cores", "--use-conda", "--configfile", "--conda-prefix"}
+    forbidden = forbidden.intersection(set(snakemake_args))
+    if forbidden:
+        raise SystemExit(f"Error: multiSMASH automatically sets the following flag"
+                         f"{'s' if len(forbidden) > 1 else ''}: {' '.join(forbidden)}")
+
+    with open(args.configfile) as yml:
+        configs = yaml.safe_load(yml)
+
+    multismash_dir = Path(__file__).parents[1]
+
+    # Validate the configfile
+    schema = multismash_dir.joinpath("workflow", "schema", "config.schema.yaml")
+    validate(configs, schema=str(schema))
+
+    if configs["run_bigscape"]:
+        install_bigscape(configs)
 
     # Get Snakefile relative to this file
-    snakefile = Path(__file__).parents[1].joinpath("workflow", "Snakefile").resolve()
+    snakefile = multismash_dir.joinpath("workflow", "Snakefile")
 
-    # We need cores from the config file for snakemake
-    with open(args.configfile) as f:
-        line = next((l for l in f if l.startswith("cores:")), None)
-    try:
-        cores = str(int(line.strip().split(":")[1]))    # Ensure int
-    except (IndexError, AttributeError, ValueError) as e:
-        raise SystemExit(
-            textwrap.dedent("Error: specify the maximum number of CPU cores to "
-                            "be used at the same time in the config file, with "
-                            "a line such as `cores: 1`")) from e
-
+    # Always install conda envs in the same location
+    conda_dir = multismash_dir.joinpath("conda")
 
     args = ["snakemake",
             "--snakefile", snakefile,
-            "--cores", cores,
+            "--cores", str(configs["cores"]),
             "--use-conda",
-            "--configfile", args.configfile]
-
+            "--configfile", args.configfile,
+            "--conda-prefix", conda_dir
+            ]
+    if configs["snakemake_flags"]:
+        args.append(configs["snakemake_flags"])
     args.extend(snakemake_args)
     subprocess.run(args)
 
---
+++ workflow/rules/common.smk
@@ -0,0 +1,85 @@
+from pathlib import Path
+
+
+def build_paths():
+    paths = {}
+    # Convert to pure paths, expand home directory (~/), and make relative to config
+    # For absolute paths, config_dir is ignored
+    config_dir = Path(workflow.configfiles[0]).parent
+    paths["IN_DIR"] = Path.resolve(config_dir / Path(config["in_dir"]).expanduser())
+    paths["OUT_DIR"] = Path.resolve(config_dir / Path(config["out_dir"]).expanduser())
+
+    # Antismash in/out
+    if not config["in_ext"]:
+        paths["AS_DIR"] = paths["IN_DIR"]
+    else:
+        paths["AS_DIR"]  = paths["OUT_DIR"] / "antismash"
+
+    # Tabulation
+    paths["COUNT"] = Path(workflow.basedir) / "scripts" / "count_regions.py"
+    paths["TABULATE"] = Path(workflow.basedir) / "scripts" / "tabulate_regions.py"
+
+    # Out directory for bigscape
+    paths["BIG_DIR"] = paths["OUT_DIR"] / "conda" / "bigscape"
+    # Pfam directory for bigscape
+    pfam = config["pfam_dir"]
+    if pfam:
+        paths["PFAM_DIR"] = Path.resolve(config_dir / Path(pfam).expanduser())
+    else:
+        paths["PFAM_DIR"] = Path(workflow.basedir).parent / "pfam"
+
+    return paths
+
+
+## Get wildcards
+
+def get_samples(paths):
+    in_dir = paths["IN_DIR"]
+    IN_EXT = config["in_ext"]
+    # antismash results as input
+    if not IN_EXT:
+        GENOMES, trash = glob_wildcards(f"{in_dir}/{{genome}}/{{trash}}.json")
+        print(f"{len(GENOMES)} antiSMASH results found")
+
+    # genome files as input
+    else:
+        GENOMES, = glob_wildcards(f"{in_dir}/{{genome}}.{IN_EXT}")
+        print(f"{len(GENOMES)} {IN_EXT} files found")
+    if not GENOMES:
+        raise OSError(f"Error! Unable to find files in '{in_dir}' "
+                      f"with extension '{IN_EXT}'.")
+
+    return GENOMES
+
+
+def get_bigscape_env():
+    env = config["bigscape_conda_env_name"]
+    if not env:
+        env = "envs/bigscape.yaml"
+
+    command = config["bigscape_command"]
+    if not command:
+        command = Path(workflow.basedir).parent / "BiG-SCAPE-1.1.5" / "bigscape.py"
+        command = f"python {command}"
+    return env, command
+
+
+def get_inputs_for_all(paths):
+    out_dir = paths["OUT_DIR"]
+    big_dir = paths["BIG_DIR"]
+    inputs = []
+    if config["run_tabulation"]:
+        inputs.extend((
+            # tabulate_regions
+            f"{out_dir}/all_regions.tsv",
+            # count_regions
+            f"{out_dir}/region_counts.tsv"
+        ))
+    # bigscape
+    if config["run_bigscape"]:
+        big_dir = out_dir / "bigscape"
+        if config["zip_bigscape"]:
+            inputs.append(f"{big_dir}.tar.gz")
+        else:
+            inputs.append(f"{big_dir}/index.html")
+    return inputs
