360596a66ecc6753559fa4245e33f7165dd104b6
Zach Reitz <zlreitz@gmail.com>
2025-04-01 10:00:10 -0700

    count: sort table by record

--- multismash/count.py
+++ multismash/count.py
@@ -44,6 +44,9 @@ def build_table(type_dict, descriptions, contig=False, split_hybrids=False):
             )
             table_list.append(this_row)
             this_row = {}
+    # Sort by record (to make my tests consistent)
+    table_list.sort(key=lambda x: x["record"])
+    
     return table_list
 
 
